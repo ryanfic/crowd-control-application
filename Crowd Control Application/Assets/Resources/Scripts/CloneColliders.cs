93ff77122389b1e4b7ce18c20f7d836c43d04e58
ryanfic <ryan_vincent_ficocelli@hotmail.com>
2020-04-27 23:22:08 -0700

    Crowd and Police Spawners added

---
+++ Crowd Control Application/Assets/Resources/Scripts/CloneColliders.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+
+public class CloneColliders : MonoBehaviour
+{
+    public float timeToWait = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        StartCoroutine(CopyAfterSeconds());
+    }
+
+    IEnumerator CopyAfterSeconds(){
+        yield return new WaitForSeconds(timeToWait); // Wait after a certain amount of time
+        Debug.Log("Finished Waiting");
+
+        //Copy all of the game objects into entities
+
+        UnityEngine.MeshCollider[] meshes = GetComponentsInChildren<UnityEngine.MeshCollider>(this.transform.gameObject);
+        foreach(UnityEngine.MeshCollider mesh in meshes){
+            CopyToEntity(mesh.gameObject);
+        }
+    }
+
+    private Entity CopyToEntity(GameObject obj){
+        EntityManager em = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        Mesh m = obj.GetComponent<UnityEngine.MeshCollider>().sharedMesh;
+        float3[] vertices = new float3[m.vertices.Length]; // a list of vertices
+        for(int i = 0; i < m.vertices.Length; i++){
+            vertices[i] = m.vertices[i]; // get the vertices
+        }
+
+        NativeArray<float3> nativeVertices = new NativeArray<float3>(vertices,Allocator.Temp);
+        NativeArray<int> nativeIndices = new NativeArray<int>(m.triangles,Allocator.Temp);
+        float3 pos = obj.transform.position;
+        Quaternion rot = obj.transform.rotation;
+
+        var mCol = Unity.Physics.MeshCollider.Create(nativeVertices,nativeIndices); // create a mesh collider from the vertices and indices
+        
+        Entity en = em.CreateEntity(typeof(Translation),typeof(Rotation),typeof(PhysicsCollider)); // Create the entity
+
+        // Add the data to the entity
+        em.SetComponentData<PhysicsCollider>(en,new PhysicsCollider{
+            Value = mCol
+        });
+        em.SetComponentData<Translation>(en, new Translation{
+            Value = pos
+        });
+        em.SetComponentData<Rotation>(en, new Rotation{
+            Value = rot
+        });
+
+        //Remove the arrays to not waste memory
+        nativeIndices.Dispose();
+        nativeVertices.Dispose();
+
+        //Set the name of the entity
+        string name = obj.name;
+        em.SetName(en,name);
+        return en;
+    }
+
+    
+}
---
+++ Crowd Control Application/Assets/Resources/Scripts/Spawners/PoliceSpawner.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PoliceSpawner : MonoBehaviour
+{
+    private float xScale;
+    private Quaternion rotation;
+    public GameObject prefab;
+
+    public string fileName = "policeLocations";
+    public int numberToSpawn = 10;
+    public float delay = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        string file = fileName + ".txt";
+        if(File.Exists(file)){ // if there is a 
+            Invoke("DataSpawnPolice",delay);
+        }
+        else{
+            Invoke("SpawnPolice",delay);
+        }
+    }
+
+    private void SpawnPolice(){
+        rotation = this.transform.rotation;
+        xScale = this.transform.localScale.x;
+
+
+        IList<Vector3> positions = new List<Vector3>();
+
+        Vector3 position;
+
+        for(int i = 0; i < numberToSpawn; i++){ // i is in the x direction
+            position = new Vector3( // create a vector based on which partition the agent is in
+                (-xScale/2)+((i+0.5f)*xScale/numberToSpawn),
+                0,
+                0
+            );
+            position = rotation * position; // rotate the vector
+            position += this.transform.position; // move the position to where the game object is
+            Object.Instantiate(prefab, position,rotation); // create the crowd agent
+
+            positions.Add(position); // add the position to the list 
+        }
+        WritePoliceData(positions);
+        RemoveGameObject();
+    }
+
+    //write the locations of all the police to a file
+    private void WritePoliceData(IList<Vector3> positions)
+    {
+        string file = fileName + ".txt";
+        StreamWriter sw = new StreamWriter(file);
+        string towrite;
+        for(int i = 0; i < positions.Count; i++){
+            //clear towrite
+            towrite = "";
+            //add location
+            //x y z separation delimited by ,
+            towrite += positions[i].x +","+positions[i].y +","+positions[i].z;
+
+            sw.WriteLine(towrite);
+        }
+        sw.Close();
+    }
+
+    private void DataSpawnPolice()
+    {
+        rotation = this.transform.rotation; // get the rotation of the parent object
+        string file = fileName + ".txt";
+        StreamReader sr = new StreamReader(file);
+        string line = "";
+        while ((line = sr.ReadLine()) != null)
+        {
+            //parse the file for data
+            //the field is location
+            //get xyz
+            string[] posString = line.Split(',');
+            float[] xyz = new float[3];
+            for(int i=0;i<posString.Length;i++)
+            {
+                xyz[i] = float.Parse(posString[i]);
+            }
+            Vector3 position = new Vector3(xyz[0],xyz[1],xyz[2]);
+
+            UnityEngine.Object.Instantiate(prefab, position,rotation); // create the police agent
+        }
+        RemoveGameObject();
+    }
+
+    //removes the script
+    private void RemoveScript()
+    {
+        Destroy(this);
+    }
+
+    private void RemoveGameObject(){
+        Destroy(this.transform.gameObject);
+    }
+}
